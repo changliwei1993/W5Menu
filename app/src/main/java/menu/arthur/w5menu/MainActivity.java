1348377220e7e46482edeae6d9d45fca2d451874
韦昌力 <774145959@qq.com>
2016-01-25 16:16:16 +0800

    三个fragment加入

--- app/src/main/java/menu/arthur/w5menu/MainActivity.java
+++ app/src/main/java/menu/arthur/w5menu/MainActivity.java
@@ -1,17 +1,69 @@
 package menu.arthur.w5menu;
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
 
+import menu.arthur.w5menu.viewpagerindicator.CirclePageIndicator;
+
+public class MainActivity extends FragmentActivity {
+    private ViewPager menu_viewpager;
+    private CirclePageIndicator mIndicator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        menu_viewpager=(ViewPager)findViewById(R.id.menu_viewpager);
+        mIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
+        ArrayList<Fragment> fragmentArrayList=new ArrayList<>();
+        for (int i=0;i<3;i++){
+            fragmentArrayList.add(new MenuListFragment());
+        }
+
+        MyViewPagerAdapter myViewPagerAdapter=new MyViewPagerAdapter(getSupportFragmentManager(),fragmentArrayList);
+        menu_viewpager.setAdapter(myViewPagerAdapter);
+        mIndicator.setViewPager(menu_viewpager);
+    }
+
+
+
+    private class MyViewPagerAdapter extends FragmentPagerAdapter {
+        private  ArrayList<Fragment> fragmentArrayList=null;
+
+        private MyViewPagerAdapter(FragmentManager fm,ArrayList<Fragment> fragmentArrayList) {
+            super(fm);
+            // TODO Auto-generated constructor stub
+            this.fragmentArrayList=fragmentArrayList;
+        }
+
+        @Override
+        public Fragment getItem(int arg0) {
+            return fragmentArrayList.get(arg0);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentArrayList.size();
+        }
+
+        @Override
+        public void notifyDataSetChanged() {
+            super.notifyDataSetChanged();
+        }
+
 
     }
 }
---
+++ app/src/main/java/menu/arthur/w5menu/MenuListFragment.java
@@ -0,0 +1,49 @@
+package menu.arthur.w5menu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by arthur on 16-1-25.
+ */
+
+
+public class MenuListFragment extends Fragment {
+    private View mMainView;
+
+
+
+    public MenuListFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        mMainView = inflater.inflate(R.layout.menu_list_fragment, (ViewGroup) getActivity().findViewById(R.id.menu_viewpager), false);
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+
+        ViewGroup p = (ViewGroup) mMainView.getParent();
+        if (p != null) {
+            p.removeAllViewsInLayout();
+        }
+        return mMainView;
+    }
+
+
+}

938db5bb584ea5d6d03a9fd738596a535c58cb69
韦昌力 <774145959@qq.com>
2016-01-25 15:21:42 +0800

    第一次提交

---
+++ app/src/main/java/menu/arthur/w5menu/MainActivity.java
@@ -0,0 +1,13 @@
+package menu.arthur.w5menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
